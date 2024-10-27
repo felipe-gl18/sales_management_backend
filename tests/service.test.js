54aaac3d7d02bc26249e90d7fccd586ce2df864b
felipe-gl18 <felipegadelha2004@gmail.com>
2024-10-27 17:32:29 -0300

    CI/CD with github

---
+++ tests/service.test.js
@@ -0,0 +1,7 @@
+import { describe, it } from "node:test";
+import assert from "node:assert";
+describe("CI/CD with github actions", () => {
+  it("should verify is 1 is equal to 1", () => {
+    assert.strictEqual(1, 1);
+  });
+});
